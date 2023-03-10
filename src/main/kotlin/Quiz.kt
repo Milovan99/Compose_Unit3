70f6021a09924e5389f0291db9e21b9fc8e05d3e
Milovan99 <milovanjakovljevic78@gmail.com>
2023-03-10 17:03:14 +0100

    Extend classes with new properties and methods

--- src/main/kotlin/Quiz.kt
+++ src/main/kotlin/Quiz.kt
@@ -18,10 +18,19 @@ class Quiz {
     val question2=Question<Boolean>("The sky is green.True or false",false, Difficulty.EASY)
     val question3=Question<Int>("How many days are between full moons?",28, Difficulty.HARD)
 
+}
 
+val Quiz.StudentProgress.progressText:String
+    get() = "$answered of $total answered"
+
+fun Quiz.StudentProgress.printProgressBar(){
+    repeat(Quiz.answered){ print("▓") }
+    repeat(Quiz.total-Quiz.answered){ print("▒") }
+    println()
+    println(Quiz.progressText)
 }
 
 fun main(){
-    println("${Quiz.answered} of ${Quiz.total} answered.")
+   Quiz.printProgressBar()
 
 }
