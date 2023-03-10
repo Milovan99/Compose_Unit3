a22f8708d0ad7ec48d59b5fe8d000a0386d83bc4
Milovan99 <milovanjakovljevic78@gmail.com>
2023-03-10 16:37:26 +0100

    Enum

--- src/main/kotlin/Quiz.kt
+++ src/main/kotlin/Quiz.kt
@@ -1,15 +1,21 @@
 class Quiz {
 
+    enum class Difficulty{
+        EASY,MEDIUM,HARD
+    }
+
     class Question<T>(
         val questionText:String,
         val answer:T,
-        val difficulty:String
+        val difficulty:Difficulty
     )
+
+
 
 }
 
 fun main(){
-    val question1=Quiz.Question<String>("Quoth the raven ___","nevermore","medium")
-    val question2=Quiz.Question<Boolean>("The sky is green.True or false",false,"easy")
-    val question3=Quiz.Question<Int>("How many days are between full moons?",28,"hard")
+    val question1=Quiz.Question<String>("Quoth the raven ___","nevermore",Quiz.Difficulty.MEDIUM)
+    val question2=Quiz.Question<Boolean>("The sky is green.True or false",false,Quiz.Difficulty.EASY)
+    val question3=Quiz.Question<Int>("How many days are between full moons?",28,Quiz.Difficulty.HARD)
 }
