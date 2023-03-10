9fd4f78b1ccd349358210b8f2cda992f11d2bd0b
Milovan99 <milovanjakovljevic78@gmail.com>
2023-03-10 19:14:10 +0100

    Maps

---
+++ src/main/kotlin/collections/Maps.kt
@@ -0,0 +1,23 @@
+package collections
+
+class Maps {
+    val solarSystem= mutableMapOf(
+        "Mercury" to 0,
+        "Venus" to 0,
+        "Earth" to 1,
+        "Mars" to 2,
+        "Jupiter" to 79,
+        "Saturn" to 82,
+        "Uranus" to 27,
+        "Neptune" to 14
+    )
+}
+
+fun main(){
+    var solarSystem = Maps().solarSystem
+    println(solarSystem.size)
+    solarSystem["Pluto"]=5
+    println(solarSystem.size)
+    println(solarSystem["Pluto"])
+    solarSystem.remove("Pluto")
+}
