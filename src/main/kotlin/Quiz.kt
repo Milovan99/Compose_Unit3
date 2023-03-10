1a2642c1c847f9493a90a7b3b49d73e15a6bc126
Milovan99 <milovanjakovljevic78@gmail.com>
2023-03-10 17:12:49 +0100

    Interface

--- src/main/kotlin/Quiz.kt
+++ src/main/kotlin/Quiz.kt
@@ -1,4 +1,11 @@
-class Quiz {
+interface ProgressPrintable{
+    val progressText:String
+    fun printProgressBar()
+}
+
+
+
+class Quiz :ProgressPrintable{
 
     enum class Difficulty{
         EASY,MEDIUM,HARD
@@ -18,19 +25,19 @@ class Quiz {
     val question2=Question<Boolean>("The sky is green.True or false",false, Difficulty.EASY)
     val question3=Question<Int>("How many days are between full moons?",28, Difficulty.HARD)
 
+    override val progressText:String
+        get() = "$answered of $total answered"
+
+    override fun printProgressBar(){
+        repeat(Quiz.answered){ print("▓") }
+        repeat(Quiz.total-Quiz.answered){ print("▒") }
+        println()
+        println(progressText)
+        }
 }
 
-val Quiz.StudentProgress.progressText:String
-    get() = "$answered of $total answered"
-
-fun Quiz.StudentProgress.printProgressBar(){
-    repeat(Quiz.answered){ print("▓") }
-    repeat(Quiz.total-Quiz.answered){ print("▒") }
-    println()
-    println(Quiz.progressText)
-}
 
 fun main(){
-   Quiz.printProgressBar()
+   Quiz().printProgressBar()
 
 }
